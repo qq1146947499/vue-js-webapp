37a9c89a8ffc6fbdb613f2fed7c2f2af6f3406f2
yijiushenjing <1146947499@qq.com>
2019-01-22 06:02:54 +0800

    准备工作完成

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <FooterGuide v-show="$route.meta.showFooter"/>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+export default {
+  components: {
+    FooterGuide
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+      .app
+        background-color #fff
+        width 100%
+        height 50px
+</style>
